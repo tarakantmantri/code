744a2f46d44bbe1b6763585acc6e8dcc364939c2
tarakantmantri <iamtarakanta@gmail.com>
2023-07-14 17:33:18 +0530

    sss

--- Udemy/Angelayu/Practise/npm/index.js
+++
@@ -1,9 +0,0 @@
-// var generate = require('sillyname');
-
-import generatName from "sillyname"
-var funnyname = generatName();
-console.log(`my Name is ${funnyname}`)
-
-import superheroes from "superheroes"
-var supername = superheroes.random();
-console.log(`I am ${supername} !`);
--- Udemy/Angelayu/Projects/11.3 TinDog Project/script.js
+++
@@ -1,4 +0,0 @@
-let message = prompt("enter your message");
-let length = message.length;
-let remain = 200 - length;
-alert("Your message is " + length + " Charecters. You have " + remain + "Charecters Remaining");
